8055d4cc6070222eff0372078d3a4107fedb91bd
informatica22023 <adrianlencina@frp.utn.edu.ar>
2026-03-11 09:23:21 -0300

    informe-pozos-txt-estaticos

---
+++ src/config/constants/muestrasAgua.informeTextos.js
@@ -0,0 +1,32 @@
+export const FACTORES_CALIDAD_AGUA = [
+{
+titulo: "1. pH",
+texto: "Nos indica el grado de acidez/alcalinidad que tiene el agua y puede indicarnos la presencia de ciertas sales en el agua como carbonatos. No es de suma importancia, lo importante es el pH final de las mezclas que hagamos con esa agua en cuestión."
+},
+
+{
+titulo: "2. Dureza",
+texto: "Está determinada por la presencia de cationes Ca+2 y Mg+2. La dureza afecta a todos los fitosanitarios, disminuyendo la estabilidad de emulsiones y suspensiones y generando separación de fases o precipitaciones."
+},
+
+{
+titulo: "3. Alcalinidad",
+texto: "Está determinada por carbonatos y bicarbonatos. Representa el buffer que posee el agua y permite entender la capacidad del agua de amortiguar acidificaciones generadas por fitosanitarios."
+},
+
+{
+titulo: "4. Salinidad",
+texto: "Es una variable importante para entender el efecto de la calidad del agua tanto al activo como a la formulación. La forma de medirla es a través de la fuerza iónica.",
+
+subsecciones: [
+    {
+        subtitulo: "La salinidad afecta de dos formas:",
+        items: [
+            "Solubilidad del ion: mayor solubilidad del ion.",
+            "Menor estabilidad de las formulaciones: las sales presentes generan un apantallamiento de la carga de los iones, perdiéndose la fuerza de repulsión y generando efectos similares a la dureza en la formulación. Las emulsiones son las más afectadas (graminicidas, S-metolacloro, insecticidas, 2-4D ácido o etil hexil)."
+        ]
+    }
+    ]
+}
+
+];
